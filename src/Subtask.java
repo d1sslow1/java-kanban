7937b25d0668ce64a9f13ae4aa42fcbf736f7ba7
d1sslow1 <artemdanielyan2006@yandex.ru>
2025-05-23 02:51:54 +0700

    Доделал ТЗ

---
+++ src/Subtask.java
@@ -0,0 +1,23 @@
+public class Subtask extends Task {
+    private final int epicId;
+
+    public Subtask(String name, String description, Status status, int epicId) {
+        super(name, description, status);
+        this.epicId = epicId;
+    }
+
+    public int getEpicId() {
+        return epicId;
+    }
+
+    @Override
+    public String toString() {
+        return "Subtask{" +
+                "name='" + getName() + '\'' +
+                ", description='" + getDescription() + '\'' +
+                ", id=" + getId() +
+                ", status=" + getStatus() +
+                ", epicId=" + epicId +
+                '}';
+    }
+}
---
+++ src/Task.java
@@ -0,0 +1,97 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Task {
+    private static final Map<Integer, Task> tasks = new HashMap<>();
+    private static int nextId = 1;
+
+    protected String name;
+    protected String description;
+    protected int id;
+    protected Status status;
+
+    public Task(String name, String description, Status status) {
+        this.name = name;
+        this.description = description;
+        this.status = status;
+        this.id = nextId++;
+        tasks.put(this.id, this);
+    }
+
+    // Статические методы для управления задачами
+    public static List<Task> getAllTasks() {
+        return new ArrayList<>(tasks.values());
+    }
+
+    public static void deleteAllTasks() {
+        tasks.clear();
+    }
+
+    public static Task getTaskById(int id) {
+        return tasks.get(id);
+    }
+
+    public static void updateTask(Task task) {
+        if (tasks.containsKey(task.id)) {
+            tasks.put(task.id, task);
+        }
+    }
+
+    public static void deleteTaskById(int id) {
+        tasks.remove(id);
+    }
+
+    // Геттеры и сеттеры
+    public int getId() {
+        return id;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Task task = (Task) o;
+        return id == task.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Integer.hashCode(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Task{" +
+                "name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", id=" + id +
+                ", status=" + status +
+                '}';
+    }
+}
