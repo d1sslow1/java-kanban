2ea26742100c150887a8ace1ff9f6aa236ae4131
d1sslow1 <artemdanielyan2006@yandex.ru>
2025-07-27 17:38:37 +0700

    Доделал ТЗ

--- src/managers/TaskManager.java
+++ src/managers/TaskManager.java
@@ -1,29 +1,52 @@
 package managers;
 
 import model.*;
-import java.util.*;
+import java.util.List;
+import java.util.Set;
 
 public interface TaskManager {
+
     List<Task> getTasks();
+
     List<Epic> getEpics();
+
     List<Subtask> getSubtasks();
+
     void deleteAllTasks();
+
     void deleteAllEpics();
+
     void deleteAllSubtasks();
+
     Task getTask(int id);
+
     Epic getEpic(int id);
+
     Subtask getSubtask(int id);
+
     int createTask(Task task);
+
     int createEpic(Epic epic);
+
     int createSubtask(Subtask subtask);
+
     void updateTask(Task task);
+
     void updateEpic(Epic epic);
+
     void updateSubtask(Subtask subtask);
+
     void deleteTask(int id);
+
     void deleteEpic(int id);
+
     void deleteSubtask(int id);
+
     List<Subtask> getEpicSubtasks(int epicId);
+
     List<Task> getHistory();
+
     Set<Task> getPrioritizedTasks();
+
     boolean isTaskOverlapping(Task newTask);
 }
